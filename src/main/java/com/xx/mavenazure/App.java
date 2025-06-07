92438b082682b2d347945ca7b34edddefc5ae713
mohitha-2004 <ushamurlimohi@gmail.com>
2025-06-07 11:01:53 +0530

    first commit

---
+++ src/main/java/com/xx/mavenazure/App.java
@@ -0,0 +1,13 @@
+package com.xx.mavenazure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
